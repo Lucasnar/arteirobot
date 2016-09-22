95da4f5ca5663b3c62570e933e1b4566951929bb
Lucas <lucasnar@gmail.com>
2016-09-22 10:32:55 -0300

    Automagic commit

--- src/main/java/bot/Model.java
+++ src/main/java/bot/Model.java
@@ -4,6 +4,7 @@ package bot;
  * Created by Aluno on 16/09/2016.
  */
 import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.FindIterable;
 import org.bson.Document;
@@ -11,22 +12,27 @@ import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Sorts.ascending;
 import static java.util.Arrays.asList;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Model {
     MongoClientURI uri;
     MongoClient mongoClient;
     MongoDatabase db;
+    MongoCollection<Document> artistsCollection;
     Model(String url){
         uri  = new MongoClientURI(url);
         mongoClient = new MongoClient(uri);
         db = mongoClient.getDatabase("arteiro");
+        artistsCollection = db.getCollection("artistas");
     }
     protected ArrayList<Artist> searchArtistName(String artistName){
         ArrayList< Artist > artistas = new ArrayList< Artist >();
         //BasicDBObject query = new BasicDBObject();
         //query.put("name", "samuel");
-        FindIterable<Document> iterable = db.getCollection("artistas")
+        FindIterable<Document> iterable = artistsCollection
                 .find(new BasicDBObject("$text", new BasicDBObject("$search", artistName))).limit(5);
+
         iterable.forEach(new Block<Document>() {
             @Override
             public void apply(final Document document) {
@@ -44,15 +50,28 @@ public class Model {
     }
 
     protected Artist showRandomArtist(){
-        BasicDBObject query = new BasicDBObject();
+        //BasicDBObject query = new BasicDBObject();
         //query.put("name", "samuel");
-        DBCollection artistsCollection = (DBCollection) db.getCollection("artistas");
-        Document artistDocument = (Document) artistsCollection.findOne();
-        Artist artist = new Artist(artistDocument.getString("name"),
-                artistDocument.getString("country"),
-                artistDocument.getString("work"),
-                artistDocument.getString("profile"));
+        Random rand = new Random();
+        int randomNum = rand.nextInt(7001);
+        final Artist[] artist = new Artist[1];
 
-        return artist;
+        FindIterable<Document> documents = artistsCollection.find();
+
+        documents.forEach(new Block<Document>() {
+            int counter = 1;
+            @Override
+            public void apply(final Document document) {
+                if(counter == randomNum){
+                    artist[0] = new Artist(document.getString("name"),
+                document.getString("country"),
+                document.getString("work"),
+                document.getString("profile"));
+                }
+                ++counter;
+            }
+        });
+
+        return artist[0];
     }
 }
