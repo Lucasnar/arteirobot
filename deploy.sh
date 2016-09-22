6ba7e10f3ef9d87123108c0feb8f021faa3a8743
Lucas <lucasnar@gmail.com>
2016-09-22 18:44:19 -0300

    Automagic commit.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 COMMIT_MESSAGE="$1"
 git add .
-if [ -z "$COMMIT_MESSAGE" ]
+if [ -z $COMMIT_MESSAGE ]
 then
-    git commit -m "$COMMIT_MESSAGE"
+    git commit -m $COMMIT_MESSAGE
 else
     git commit -m "Automagic commit."
 fi
